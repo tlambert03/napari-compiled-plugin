6334aee875cc087f587118987a6cdfd2cd8a03d0
Talley Lambert <talley.lambert@gmail.com>
2022-08-06 07:38:59 -0700

    initial commit

---
+++ src/napari_compiled_plugin/_tests/test_sample_data.py
@@ -0,0 +1,7 @@
+# from napari_compiled_plugin import make_sample_data
+
+# add your tests here...
+
+
+def test_something():
+    pass
---
+++ src/napari_compiled_plugin/_tests/test_writer.py
@@ -0,0 +1,7 @@
+# from napari_compiled_plugin import write_single_image, write_multiple
+
+# add your tests here...
+
+
+def test_something():
+    pass
